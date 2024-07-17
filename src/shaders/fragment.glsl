92412180a54b92b67072dad8cccabe0e739a05dc
Tri <trimpham98@gmail.com>
2024-07-16 23:07:42 -0700

    Add formulas for atmosphere shaders

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,17 @@
-uniform vec2 u_resolution;
+// reference from https://youtu.be/vM8M4QloVL0?si=CKD5ELVrRm3GjDnN
+varying vec3 vNormal;
+varying vec3 eyeVector;
+uniform float atmOpacity;
+uniform float atmPowFactor;
+uniform float atmMultiplier;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-
-  gl_FragColor = vec4(vec3(1.0, uv), 1.0);
-
-  // transform color from linear colorSpace to sRGBColorSpace
-  gl_FragColor = linearToOutputTexel( gl_FragColor );
+    // Starting from the rim to the center at the back, dotP would increase from 0 to 1
+    float dotP = dot( vNormal, eyeVector );
+    // This factor is to create the effect of a realistic thickening of the atmosphere coloring
+    float factor = pow(dotP, atmPowFactor) * atmMultiplier;
+    // Adding in a bit of dotP to the color to make it whiter while the color intensifies
+    vec3 atmColor = vec3(0.35 + dotP/4.5, 0.35 + dotP/4.5, 1.0);
+    // use atmOpacity to control the overall intensity of the atmospheric color
+    gl_FragColor = vec4(atmColor, atmOpacity) * factor;
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,15 @@
+varying vec3 vNormal;
+varying vec3 eyeVector;
+
 void main() {
     // modelMatrix transforms the coordinates local to the model into world space
-    vec4 worldPos = modelMatrix * vec4(position, 1.0);
-    // viewMatrix transform the world coordinates into the world space viewed by the camera (view space)
-    vec4 mvPosition = viewMatrix * worldPos;
+    vec4 mvPos = modelViewMatrix * vec4( position, 1.0 );
 
-    gl_Position = projectionMatrix * mvPosition;
+    // normalMatrix is a matrix that is used to transform normals from object space to view space.
+    vNormal = normalize( normalMatrix * normal );
+
+    // vector pointing from camera to vertex in view space
+    eyeVector = normalize(mvPos.xyz);
+
+    gl_Position = projectionMatrix * mvPos;
 }
